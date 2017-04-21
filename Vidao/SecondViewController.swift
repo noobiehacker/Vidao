b2a9a512fb99b20184f42df362c5275eb67e23c6
David Chan <davidkwokhochan@gmail.com>
2017-04-21 15:57:31 -0700

    Initial Commit

---
+++ Vidao/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Vidao
+//
+//  Created by David Kwok Ho Chan on 4/21/17.
+//  Copyright © 2017 David Kwok Ho Chan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
